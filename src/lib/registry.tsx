03bb40d1de3a4ba8bf18b3c210d95df58f1d3c23
velo1203 <hokitoki1203@gmail.com>
2025-03-23 22:26:08 +0900

    rebuild

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -9,8 +9,6 @@ export default function StyledComponentsRegistry({
 }: {
     children: React.ReactNode;
 }) {
-    // Only create stylesheet once with lazy initial state
-    // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
     const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
     useServerInsertedHTML(() => {
